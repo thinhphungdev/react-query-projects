633cac12a584c1b3449ae1d3430568a76b88015a
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-21 23:17:05 +0700

    useMutation to update and delete post

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -29,13 +29,35 @@ export function PostDetail({ post }) {
     () => fetchComments(post.id)
   );
 
+  const {
+    mutate: deletePostMutate,
+    isError: deletePostError,
+    isLoading: isDeletingPost,
+  } = useMutation((postId) => deletePost(postId));
+
+  const {
+    mutate: updatePostMutate,
+    isError: updatePostError,
+    isLoading: isUpdating,
+  } = useMutation((postId) => updatePost(postId));
+
   if (isLoading) return <h3>Loading...</h3>;
   if (isError) return <div>Something went wrong {error.toString()}</div>;
 
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deletePostMutate(post.id)}>Delete</button>{' '}
+      {deletePostError && (
+        <div style={{ color: 'red' }}>Error deleting post</div>
+      )}
+      {isDeletingPost && <div style={{ color: 'red' }}>Deleting Post</div>}
+      {/*  */}
+      <button onClick={() => updatePostMutate(post.id)}>Update title</button>
+      {updatePostError && (
+        <div style={{ color: 'red' }}>Error deleting post</div>
+      )}
+      {isUpdating && <div style={{ color: 'red' }}>Deleting Post</div>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
