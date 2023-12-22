b8609f13831a6ecf0b64adf5f0f1b4a2a844f3c8
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-22 10:59:28 +0700

    init infinite scroll

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,8 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/';
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,7 +1,8 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Species } from './Species';
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/';
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
