54186078419b97cfcd86e73dac57f7dcc72994da
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-23 15:14:32 +0700

    useStaff challenge

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { Dispatch, SetStateAction, useState } from "react";
 
 import type { Staff } from "@shared/types";
@@ -7,11 +8,12 @@ import { filterByTreatment } from "../utils";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
+
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -20,11 +22,14 @@ interface UseStaff {
 }
 
 export function useStaff(): UseStaff {
-  // for filtering staff by treatment
-  const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff = [];
+  const [filter, setFilter] = useState("all");
+  const falllback = [];
+
+  const { data: staff = falllback } = useQuery({
+    queryKey: queryKeys.staff,
+    queryFn: getStaff,
+  });
 
   return { staff, filter, setFilter };
 }
