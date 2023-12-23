3a46ba401c98d909556f4fb15ff56fd9f0099be4
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-23 15:04:13 +0700

    centralized loading indicator using useIsFetching

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,22 +1,28 @@
 import { QueryClient } from "@tanstack/react-query";
 
-// import { toast } from "@/components/app/toast";
+import { toast } from "@/components/app/toast";
 
-// function errorHandler(type: "query" | "mutation", errorMsg: string) {
-//     // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//     // one message per page load, not one message per query
-//     // the user doesn't care that there were three failed queries on the staff page
-//     //    (staff, treatments, user)
-//     const id = "react-query-toast";
+function errorHandler(type: "query" | "mutation", errorMsg: string) {
+    // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+    // one message per page load, not one message per query
+    // the user doesn't care that there were three failed queries on the staff page
+    //    (staff, treatments, user)
+    const id = "react-query-toast";
 
-//     if (!toast.isActive(id)) {
-//       const action = type === "query" ? "load" : "update";
-//       const title = `could not ${action} data: ${
-//         errorMsg ?? "error connecting to server"
-//       }`;
-//       toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//     }
-//   }
+    if (!toast.isActive(id)) {
+        const action = type === "query" ? "load" : "update";
+        const title = `could not ${action} data: ${errorMsg ?? "error connecting to server"
+            }`;
+        toast({ id, title, status: "error", variant: "subtle", isClosable: true });
+    }
+}
 
-// to satisfy typescript until this file has uncommented contents
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            throwOnError(error) {
+                errorHandler(error, error.message)
+            },
+        }
+    }
+});
