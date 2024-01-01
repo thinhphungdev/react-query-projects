9c2c83d4cdbe00bde5eb033813daf9a5e232bf8f
thinhphungdev <phungthinh0946519356@gmail.com>
2024-01-01 15:35:01 +0700

    using usemutation

--- base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,3 +1,5 @@
+import { UseMutateFunction, useMutation } from "@tanstack/react-query";
+
 import { Appointment } from "@shared/types";
 
 import { useLoginData } from "@/auth/AuthContext";
@@ -6,28 +8,24 @@ import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
 
 // for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined,
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? 'replace' : 'add';
+  const patchData = [{ op: patchOp, path: '/userId', value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
-// TODO: update type for React Query mutate function
-type AppointmentMutationFunction = (appointment: Appointment) => void;
-
-export function useReserveAppointment(): AppointmentMutationFunction {
+export function useReserveAppointment(): UseMutateFunction<void, unknown, Appointment, unknown> {
   const { userId } = useLoginData();
 
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation((appointment: Appointment) => setAppointmentUser(appointment, userId));
+
+  return mutate;
 }
